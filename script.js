bf7d2625a40d2dbf554a4e903e8644e56b3b0bf8
SolomonOzor <157052930+SolomonOzorSectionE@users.noreply.github.com>
2026-03-22 20:50:42 -0400

    Add PerishableProduct class with inheritance

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ const product1 = new Product("Apple", 2.5, 50);
 
 console.log(product1.toString());
 console.log("Total Value:", product1.getTotalValue());
+
+
+
+// PART 2: ADDING INHERITANCE
+// PerishableProduct Class that inherits all properties from Product
+class PerishableProduct extends Product {
+  constructor(name, price, quantity, expirationDate) {
+    super(name, price, quantity);
+
+    // New property
+    this.expirationDate = expirationDate;
+  }
+
+  // Override toString method to include expiration date
+  toString() {
+    return `Product: ${this.name}, Price: $${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
+  }
+}
